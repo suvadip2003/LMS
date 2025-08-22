15eaf4b1f7c30fddd47748d50d274a3814b02164
Suvadip <yosuvadip@gmail.com>
2025-08-22 09:32:54 +0530

    Update server URL to use the production endpoint

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -26,7 +26,7 @@ import ViewLecture from './pages/ViewLecture'
 import SearchWithAi from './pages/SearchWithAi'
 import getAllReviews from './customHooks/getAllReviews'
 
-export const serverUrl = "http://localhost:8000"
+export const serverUrl = "https://lms-2lt2.onrender.com"
 
 function App() {
   
